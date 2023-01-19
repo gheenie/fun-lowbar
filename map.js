e27c0349e1a64d5302cd60518790898240c38288
infoomics <sbx_94@hotmail.com>
2023-01-19 18:19:36 +0000

    completed 3rd and 4th test case

--- map.js
+++ map.js
@@ -1,12 +1,26 @@
 function map(collection, iterator)
 {
+    const collectionIsArray = Array.isArray(collection);
+    const collectionIsObject = !collectionIsArray && typeof collection === 'object' && collection !== null;
+    const iteratorIsString = typeof iterator === 'string'
+    
     const mappedArray = [];
 
-    for (let i = 0; i < collection.length; i++)
-    {
-        mappedArray.push( iterator(collection[i]) );
+    if (collectionIsArray) {
+        for (let i = 0; i < collection.length; i++) {
+            mappedArray.push( iterator(collection[i]) );
+        }
     }
-    
+
+    if (collectionIsObject) {
+        const collectionValues = Object.values(collection);
+        
+        for (let i = 0; i < collectionValues.length; i++) {
+            mappedArray.push( iterator(collectionValues[i]) );
+        }
+    }
+
     return mappedArray;
 }
+
 module.exports = map;
