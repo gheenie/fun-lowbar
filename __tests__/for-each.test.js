a685926c452b265cf6536cc8a008ced46f9a8aec
infoomics <sbx_94@hotmail.com>
2023-01-19 20:36:43 +0000

    made test suites for non-return functions

--- __tests__/for-each.test.js
+++ __tests__/for-each.test.js
@@ -1,30 +1,85 @@
 const forEach = require('../for-each');
 
-function double(n) {
-    return n * 2;
-}
-
-function combineValueKey(value, key) {
-    return value + key;
-}
-
 describe('happy paths, function returns value', () => {
+    function double(n) {
+        return n * 2;
+    }
+    
+    function combineValueKey(value, key) {
+        return value + key;
+    }
+    
     test('input1 is array, input2 is one-argument func', () => {
-        const input = [1, 2];
+        const input1 = [1, 2];
+        const input2 = double;
 
-        const output = forEach(input, double);
+        const output = forEach(input1, input2);
 
         const expected = [2, 4];
         expect(output).toEqual(expected);
     });
 
     test('input1 is object, input2 is two-argument func', () => {
-        const input = { 'a': 1, 'b': 2 };
+        const input1 = { 'a': 1, 'b': 2 };
+        const input2 = combineValueKey;
 
         // unguaranteed iteration order. sort() to make assertion easier.
-        const output = forEach(input, combineValueKey);
+        const output = forEach(input1, input2).sort();
 
         const expected = ['1a', '2b'];
+        expect(output).toEqual(expected);
+    });
+});
+
+describe('happy paths, function does not return value', () => {
+    test('input1 is array, input2 is one-argument func', () => {
+        const input1 = [1, 2];
+        const output = [];
+        function input2(x) {
+            output.push(x);
+        }
+
+        forEach(input1, input2);
+
+        const expected = [1, 2];
+        expect(output).toEqual(expected);
+    });
+
+    test('input1 is object, input2 is two-argument func', () => {
+        const input1 = { 'a': 1, 'b': 2 };
+        const output = [];
+        function input2(x, y) {
+            output.push(x);
+            output.push(y);
+        }
+
+        forEach(input1, input2);
+
+        const expected = [1, 'a', 2, 'b'];
+        expect(output).toEqual(expected);
+    });
+});
+
+describe('happy paths, function does not return value, use mock functions', () => {
+    test('input1 is array, input2 is one-argument func', () => {
+        const input1 = [1, 2];
+        const mockFunc = jest.fn();
+
+        forEach(input1, mockFunc);
+        const output = mockFunc.mock.calls;
+
+        const expected = [[1], [2]];
+        expect(output).toEqual(expected);
+    });
+
+    test('input1 is object, input2 is two-argument func', () => {
+        const input1 = { 'a': 1, 'b': 2 };
+        const mockFunc = jest.fn();
+
+        forEach(input1, mockFunc);
+        const output = mockFunc.mock.calls;
+
+        const expected = [[1, 'a'], [2, 'b']];
         expect(output).toEqual(expected);
     });
 });
