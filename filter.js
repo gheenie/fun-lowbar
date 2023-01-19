a7a069c49d60be884f84fcc675d1081780cbf053
Zayn107 <ahmadasd01@hotmail.com>
2023-01-19 15:25:38 +0000

    test 3 completed

--- filter.js
+++ filter.js
@@ -3,10 +3,22 @@ const map = require('./map');
 function filter(arr, condition) {
     const m = 'empty';
     if (arr.length === 0) return m;
-    
     const resultarr = [];
-    
-    if (typeof condition === 'object' && condition !== null)
+    if(Array.isArray(condition))
+    {
+        for (let i = 0 ; i < arr.length ; i++)
+        {
+            const values = arr[i][condition[0]];
+           /* const values = arr[i][condition[1]];
+            console.log(key);*/
+            console.log(values);
+            if(arr[i].hasOwnProperty(condition[0])&&
+            values === condition[1]/*&&values===condition[1]*/)
+            resultarr.push(arr[i]);
+            console.log(resultarr,' line 15');
+        }       
+    }
+    else if (typeof condition === 'object' && condition !== null)
     {
         const conditionKeys = Object.keys(condition);
         const conditionValues = Object.values(condition);
@@ -27,7 +39,8 @@ function filter(arr, condition) {
             
             if (jPassed === conditionLength) resultarr.push(arr[i]);
         }
-    } else {
+    }
+     else {
         for (let i = 0 ; i < arr.length ; i++)
         {
             if ( condition(arr[i]) ) resultarr.push(arr[i]);
